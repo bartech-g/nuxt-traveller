d13a215e30e61d11d8f894ceaa85308d5d5202fd
bartech-g <bartech.js@gmail.com>
2025-06-19 14:07:33 +0200

    Create landing page

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar bg-base-100 shadow-sm fixed top-0 z-50">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div class="flex-none">
+      <button class="btn btn-ghost">
+        Sign In
+      </button>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Hello there
+        </h1>
+        <p class="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+          quasi. In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+        <button class="btn btn-secondary">
+          Sign In With GitHub
+          <Icon
+            name="tabler:brand-github"
+
+            size="24"
+          />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
